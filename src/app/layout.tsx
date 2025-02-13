f7f1b8d987912acebcedb02448e791bcbc61af35
Florian Braun <Braunf25@proton.me>
2025-02-13 10:32:33 +0100

    Adding Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
+        {children}
+        <Analytics />
       </body>
     </html>
   );
